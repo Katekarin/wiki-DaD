8d5b70f49621812d2a84cbfdf96daac5ea462e2b
Katekarin <94514885+Katekarin@users.noreply.github.com>
2025-06-18 23:48:09 +0200

    Update dungeons.js

--- data/dungeons.js
+++ data/dungeons.js
@@ -1,27 +1,32 @@
 const dungeons = [
   {
-    name: "Dark Chamber",
-    type: "dungeon",
-    description: "Player loses 1 turn and discovers a trap."
-  },
-  {
     name: "Poisonous Swamp",
     type: "dungeon",
-    description: "Reduces player’s agility by 2 for 3 turns."
+    description: "Its an ambush! First player that enters this, starts fight with Fishbeast."
   },
   {
     name: "Haunted Library",
     type: "dungeon",
-    description: "Player gains knowledge but loses 1 health."
+    description: "Player once can spend whole turn of not doing anything here to start combat with ghost. if wins, gain 1 WIS."
   },
   {
     name: "Cursed Altar",
     type: "dungeon",
-    description: "Player must sacrifice an item or lose 5 health."
+    description: "Player must sacrifice an item from his EQ or lose 5 health."
   },
   {
     name: "Forgotten Crypt",
     type: "dungeon",
-    description: "Enemies ambush the player, starting combat immediately."
-  }
+    description: "Spawns a zombie every 2 rounds to maximum 3 in the dungeon. upon enterning, start a fight with a zombie, as a reward drow 1 random item"
+  },
+  {
+    name: "underground cemetery",
+    type: "dungeon",
+    description: "Spawns a skeletone every 2 rounds to maximum 3 in the dungeon. upon enterning, start a fight with a skeleton, as a reward drow 1 random item"
+  },
+  {
+    name: "passage",
+    type: "dungeon",
+    description: ""
+  },
 ];
