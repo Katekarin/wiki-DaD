4cd440817da7eb6afdc71e8c5e2ed30c6f6027c2
Katekarin <94514885+Katekarin@users.noreply.github.com>
2025-06-18 23:47:38 +0200

    Update equipment.js

--- data/equipment.js
+++ data/equipment.js
@@ -2,43 +2,81 @@ const equipment = [
   {
     name: "Leather Armor",
     type: "equipment",
+    slot: "armor",
     description: "Increases health by 3 points.",
+    stats:  {
+      health: 3 
+    },
     image: "leather_armor"
   },
   {
     name: "Steel Sword",
     type: "equipment",
+    slot: "weapon",
     description: "Increases attack strenght by 1 point.",
+    stats:  {
+      STR: 1 
+    },
     image: "steel_sword"
   },
   {
     name: "Leather Boots",
     type: "equipment",
+    slot: "boots",
     description: "Increases dexterity by 1 point.",
+    stats:  {
+      DEX: 1
+    },
     image: "leather_boots"
   },
   {
     name: "Shield of Light",
     type: "equipment",
+    slot: "shield",
     description: "Reduces incoming damage by 1 point.",
+    stats:  {
+      
+    },
     image: "shield_of_light"
   },
   {
     name: "Mage's Tome",
     type: "equipment",
+    slot: "amulet",
     description: "Increases starting hand by 1 card.",
+    stats:  {
+      
+    },
     image: "mages_robe"
   },
   {
     name: "Ring of fire exposure",
     type: "equipment",
+    slot: "ring",
     description: "Your fire spells deal adicional 1damage at the end of monster turn.",
+    stats:  {
+      
+    },
     image: "Ring_of_fire_exposure"
   },
   {
     name: "Ring of ice ​​influence",
     type: "equipment",
+    slot: "ring",
     description: "Your frost spells are AOE.",
+    stats:  {
+      
+    },
     image: "Ring_of_ice_​​influence"
+  },
+  {
+    name: "explosive sword",
+    type: "equipment",
+    slot: "weapon",
+    description: "When you attack, deal 1 damage to all enemies. <br> Increases attack strenght by 2 points",
+    stats:  {
+      STR: 2 
+    },
+    image: "explosive_sword"
   }
 ];
